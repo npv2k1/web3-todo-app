0194d29e6142a01633d777e1b6af46369c1aad45
npv2k1 <npv2k1@gmail.com>
2022-08-06 19:20:49 +0700

    update next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   swcMinify: true,
   reactStrictMode: true,
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
